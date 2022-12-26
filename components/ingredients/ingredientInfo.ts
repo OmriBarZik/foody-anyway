9db1a252ab477aa9438979656fc665c7c7ae276a
InbarDanieli <inbar0308@gmail.com>
2022-12-26 12:15:56 +0200

    feat: admin page creation

---
+++ components/ingredients/ingredientInfo.ts
@@ -0,0 +1,14 @@
+export function getIngredients() {
+  return [
+    { _id: 1, name: "banana" },
+    { _id: 2, name: "apple" },
+    { _id: 3, name: "chocolate" },
+    { _id: 4, name: "lemon" },
+    { _id: 5, name: "milk" },
+    { _id: 6, name: "bread" },
+    { _id: 7, name: "salt" },
+    { _id: 8, name: "peper" },
+    { _id: 9, name: "rice" },
+    { _id: 10, name: "souce" }
+  ]
+}
---
+++ styles/Home-layout.js
@@ -0,0 +1,3 @@
+import { css } from '@emotion/css';
+
+export const homeLayout=css`background:red`
