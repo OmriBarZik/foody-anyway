e6e8515d773177945c815385e58cbb3485249615
InbarDanieli <inbar0308@gmail.com>
2023-01-13 12:12:13 +0200

    feat: improve signin page

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,52 @@
-import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import type { NextAuthOptions, Session } from "next-auth";
+import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
+// import type { GenericObject, SessionBase } from "next-auth/utils/logger";
 
-export default NextAuth({
+const scopes = [
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/userinfo.profile",
+];
+
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+          scope: scopes.join(" "),
+        },
+      },
     }),
   ],
+  debug: process.env.NODE_ENV === "development",
+
+  callbacks: {
+    async jwt({ token, user, account, isNewUser, profile }): Promise<JWT> {
+      if (account) {
+        token.accessToken = account?.accessToken;
+        token.refreshToken = account?.refreshToken;
+      }
+
+      console.log(token, account);
+
+      return token;
+    },
+    async session({ session, user }): Promise<Session> {
+      console.log(session);
+      return session;
+    },
+  },
+
   pages: {
     signIn: "/login",
   },
-});
+};
+
+export default NextAuth(authOptions);
