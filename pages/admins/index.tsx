ee4c62dbe3944596a49383ea621fc066fbae7d97
InbarDanieli <inbar0308@gmail.com>
2024-02-02 14:19:09 +0200

    WIP

--- pages/admins/index.tsx
+++ pages/admins/index.tsx
@@ -1,61 +1,89 @@
-import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import {
+  Box,
+  Chip,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  MenuProps,
+  OutlinedInput,
+  Select,
+  TextField,
+} from "@mui/material";
 import { useState } from "react";
 import useSWR from "swr";
-import { shareInfoType } from "../../components/context";
 import { fetcher } from "../../components/fetcher";
 import { Ingredient } from "../../models";
 import { container } from "../../styles/admins/index.style";
 
-export default function Testy() {
+export default function RecipesManagment() {
   const [stepsArr, setStepsArr] = useState<string[]>([""]);
-  const [IngredientsArr, setIngredientsArr] = useState<string[]>([]);
+  // const [IngredientsArr, setIngredientsArr] = useState<string[]>([]);
   const [title, setTitle] = useState<string>("");
-  const [url, setURL] = useState<string>("");
-
-  const shareInfo: shareInfoType = {
-    stepsArr,
-    setStepsArr,
-    IngredientsArr,
-    setIngredientsArr,
-    title,
-    setTitle,
-    url,
-    setURL,
-  };
-
   const { data, error, isLoading } = useSWR<Ingredient[]>(
     "/api/ingredients",
     fetcher
   );
 
+  const [selectedIngredions, setSelectedIngredions] = useState<string[]>([]);
+
   return (
     <div css={container}>
-      {isLoading && <p>loading...</p>}
-
-      {error && <div>failed to load</div>}
-
-      {data && (
-        <Box sx={{ minWidth: 120 }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Ingredient</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              // value={age}
-              label="Ingredient"
-              // onChange={handleChange}
-            >
-              {data.map((ingredient, i: number) => (
-                <MenuItem
-                  key={ingredient.name + i}
-                  value={ingredient._id?.toString()}
-                >
-                  {ingredient.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+      <FormControl>
+        <Box
+          sx={{
+            width: 500,
+            maxWidth: "100%",
+          }}
+        >
+          <TextField
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            fullWidth
+            label="Title"
+            id="fullWidth"
+          />
         </Box>
+      </FormControl>
+
+      {isLoading && <p>loading...</p>}
+      {error && <div>failed to load ingredions </div>}
+      {data && (
+        <FormControl>
+          <InputLabel id="demo-multiple-chip-label">Ingredient</InputLabel>
+          <Select
+            sx={{ minWidth: 300 }}
+            labelId="demo-multiple-chip-label"
+            id="demo-multiple-chip"
+            multiple
+            value={selectedIngredions}
+            input={<OutlinedInput label="Ingrediants" />}
+            onChange={(e) => setSelectedIngredions(e.target.value as string[])}
+            renderValue={(selected) => (
+              <Box
+                sx={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  gap: 0.5,
+                  width: 300,
+                  minWidth: 300,
+                }}
+              >
+                {selected.map((value, i) => (
+                  <Chip key={value + i} label={value} />
+                ))}
+              </Box>
+            )}
+            MenuProps={
+              data.map((e) => ({} as Partial<MenuProps>)) as Partial<MenuProps>
+            }
+          >
+            {data.map((value) => (
+              <MenuItem key={value._id?.toString()} value={value.name}>
+                {value.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       )}
     </div>
   );
